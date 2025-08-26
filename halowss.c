16c69514cdde83f99df33113ee930ced4e05424f
RavindharGundoji <155251339+RavindharGundoji@users.noreply.github.com>
2025-08-26 22:40:16 +0530

    Add files via upload

---
+++ halowss.c
@@ -0,0 +1,22 @@
+// 72.hallow square shape
+#include<stdio.h>
+
+int main()
+{
+ int i,j,n;
+
+ printf("enter no of rows :");
+ scanf("%d",&n);
+ 
+ for(i=1;i<=n;i++)
+ {
+  for(j=1;j<=n;j++)
+    {
+     if(i==1 || i==n || j==1 || j==n)
+       printf("*");
+      else
+        printf(" ");
+    }
+    printf("\n");
+ }
+}
