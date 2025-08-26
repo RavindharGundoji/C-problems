43d07f058d0b743795368c0121822d230433c244
RavindharGundoji <155251339+RavindharGundoji@users.noreply.github.com>
2025-08-26 14:27:10 +0530

    Add files via upload

---
+++ E65.c
@@ -0,0 +1,20 @@
+// sum of all even numbers bw two given numbers using loops and if else.
+#include<stdio.h>
+int main()
+{ 
+ int start ,end,i,sum=0;
+ printf("Enter the first number : ");
+ scanf("%d",&start);
+ printf("Enter the second number : ");
+ scanf("%d",&end);
+ 
+ for(i=start;i<=end;i++){
+    if(i%2 == 0)
+    {
+       sum+=i;
+     }
+ }
+ 
+ printf("sum of even numbers bw %d and %d is: %d\n",start,end,sum);
+ return 0;      
+}
