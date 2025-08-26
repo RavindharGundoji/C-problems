002c8252486ddccd35ad97ddd249e2d24326580b
RavindharGundoji <155251339+RavindharGundoji@users.noreply.github.com>
2025-08-26 21:06:18 +0530

    Add files via upload

---
+++ areaofacircle.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#define PI  3.414
+
+int main(){
+   int r,i,d;
+   scanf("%d",&r);
+   float area;
+   
+   for(i=1;i == 1;i++)
+   {
+    d=r*r;
+    break;
+   }
+  
+  area = PI*d;
+  
+  printf("area of a circle with radius %d = %.2f is :",r,area);
+  return 0;
+}
