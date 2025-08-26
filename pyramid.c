4a768936a63c5d9c51d19ec79b7f9072c3db0bb6
RavindharGundoji <155251339+RavindharGundoji@users.noreply.github.com>
2025-08-26 23:26:35 +0530

    Add files via upload

---
+++ pyramid.c
@@ -0,0 +1,23 @@
+// 75.pyramid shape
+#include<stdio.h>
+
+int main()
+{
+ int i,j,n;
+
+ printf("enter no of rows :");
+ scanf("%d",&n);
+ 
+ for(i=1;i<=n;i++)
+ {
+  for(j=n;j>=1;j--)
+    {
+     if(i>=j)
+        printf("* ");  // space is imp
+      else 
+        printf(" ");
+    }
+     printf("\n");
+ }
+ return 0;
+}
